c6ff4ea5f562dc8d9ac512ce35af136c666724a1
ziaasifmuhammad <155935485+ziaasifmuhammad@users.noreply.github.com>
2025-02-06 00:53:12 +0500

    Add files via upload

---
+++ sanity.config.ts
@@ -0,0 +1,26 @@
+'use client'
+
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
+ */
+
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './src/sanity/env'
+import {schema} from './src/sanity/schemaTypes'
+import {structure} from './src/sanity/structure'
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId: projectId as string,
+  dataset: dataset as string,
+  schema,
+  plugins: [
+    structureTool({structure}),
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+});
+
